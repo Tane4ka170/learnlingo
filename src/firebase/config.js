2bb6eda9a896ef9aa31186e0ea088ce81dc4b4dc
Tane4ka170 <tane4ka170@gmail.com>
2024-04-08 23:02:13 +0300

    init

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzbjN91v8IWAIkQi9j2kP7PaXuCofX6xk",
+  authDomain: "learnlingo-56b2c.firebaseapp.com",
+  projectId: "learnlingo-56b2c",
+  storageBucket: "learnlingo-56b2c.appspot.com",
+  messagingSenderId: "45829146938",
+  appId: "1:45829146938:web:99e545fc416c95a1c2e803",
+  measurementId: "G-1MCP71X14M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+
+export const auth = getAuth(app);
