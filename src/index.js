352da4418ebd535a2b3b96a615cc56cbf69d9dd6
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-04-12 22:53:31 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
-import theme from "./theme.js";
-import { GlobalStyle } from "./GlobalStyle.js";
-import { ThemeProvider } from "styled-components";
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
+import theme from './theme.js';
+import { GlobalStyle } from './GlobalStyle.js';
+import { ThemeProvider } from 'styled-components';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/">
     <Provider store={store}>
       <ThemeProvider theme={theme}>
