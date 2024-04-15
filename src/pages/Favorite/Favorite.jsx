71f979cc8f73e1c5e8f142149b37aba9c752f6a1
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-04-15 21:44:20 +0300

    Update Favorite

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,54 +1,55 @@
-import CardList from 'components/CardList/CardList';
-import { Loader } from 'components/Loader/Loader';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTeachersAsync } from '../../redux/teachers/operations';
+import { Loader } from 'components/Loader/Loader';
+import Card from 'components/Card/Card';
 
 const Favorite = ({ authUser }) => {
   const dispatch = useDispatch();
-
   const isLoadingTeachers = useSelector(state => state.teachers.isLoading);
-  const teachers = useSelector(state => state.teachers.teachers);
+  const teachers = useSelector(state => state.teachers.items);
   const favoriteList = useSelector(state => state.favorite.favorite);
-
   const [loadedTeachersCount, setLoadedTeachersCount] = useState(4);
 
-  const favoriteTeachers = teachers?.filter(teacher =>
-    favoriteList.includes(teacher.id)
-  );
+  useEffect(() => {
+    if (teachers.length === 0) {
+      dispatch(fetchTeachersAsync());
+    }
+  }, [dispatch, teachers]);
+
+  const favoriteTeachers =
+    teachers?.length > 0
+      ? teachers.filter(teacher => favoriteList.includes(teacher.id))
+      : [];
   const noFavoriteTeachers =
     !isLoadingTeachers && (!favoriteTeachers || favoriteTeachers.length === 0);
-
-  useEffect(() => {
-    dispatch(fetchTeachersAsync());
-  }, [dispatch]);
 
   const handleLoadMore = () => {
     setLoadedTeachersCount(prevCount => prevCount + 4);
   };
 
+  if (isLoadingTeachers || teachers.length === 0) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+
   return (
     <div>
-      {isLoadingTeachers && (
-        <div>
-          <Loader />
-        </div>
-      )}
-
       {noFavoriteTeachers && (
         <div>
           <p>There are no favorite teachers yet</p>
         </div>
       )}
-
       {favoriteTeachers && favoriteTeachers.length > 0 && (
         <ul>
           {favoriteTeachers.slice(0, loadedTeachersCount).map(teacher => (
-            <CardList key={teacher.id} teacher={teacher} authUser={authUser} />
+            <Card key={teacher.id} teacher={teacher} authUser={authUser} />
           ))}
         </ul>
       )}
-
       {favoriteTeachers &&
         favoriteTeachers.length > loadedTeachersCount &&
         !isLoadingTeachers && (
