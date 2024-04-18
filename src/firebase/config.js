fd3b45eeebbb17d68e76790950f49c81a259da92
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-04-18 20:35:09 +0300

    changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,7 +5,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: 'https://learnlingo-56b2c-default-rtdb.firebaseio.com',
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
